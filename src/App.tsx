715779518ecd141f8f0d8f03f4671bbf19b47d4e
amalreji111 <amalreji111@gmail.com>
2023-06-06 18:45:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+/**
+ * 
+ * COntain name,address,pincode and a dropdown box active and inactive in the list
+ * should be an action column (edit and delete),top of th
+ * 
+ * 
+ */
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
